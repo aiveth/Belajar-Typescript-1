73094866ed00c790433f8a9c2153489a8a188c80
RSurya99 <rafli.rfsp@gmail.com>
2021-12-27 16:41:25 +0700

    change function name

--- rafli-surya-latihan-typescript/namafungsi.ts
+++ rafli-surya-latihan-typescript/namafungsi.ts
@@ -2,9 +2,9 @@ import { article } from './article.response'
 import { Data } from './type.article'
 
 // buat function untuk memanggil article
-function callArticle(data: Data[]): Data[] {
+function getPublishedArticle(data: Data[]): Data[] {
   return data.filter((article: Data) => article.attributes.published === false)
 }
 
-// panggil function callArticle
-console.log(callArticle(article.data))
+// panggil function getPublishedArticle
+console.log(getPublishedArticle(article.data))
